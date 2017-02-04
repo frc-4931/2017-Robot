367f57689f3cc839ebf37cfc19fb30a541180544
Adam Gausmann <adam@nonemu.ninja>
2017-02-04 10:51:45 -0600

    Add JavaDoc to Drivetrain

--- CompetitionRobot/src/org/frc4931/robot/drive/Drivetrain.java
+++ CompetitionRobot/src/org/frc4931/robot/drive/Drivetrain.java
@@ -15,22 +15,47 @@ public class Drivetrain implements Requirable {
         this.heading = heading;
     }
 
+    /**
+     * Provides a field-oriented control scheme that accepts raw joystick inputs.
+     * @param x The joystick's X (roll) axis, a value in [-1.0..1.0]. Translates to horizontal motion on the field.
+     * @param y The joystick's Y (pitch) axis, a value in [-1.0..1.0]. Translates to motion toward/away the driver station.
+     * @param rotation The joystick's twist (yaw) axis, a value in [-1.0..1.0]. Translates to rotation of the robot.
+     */
     public void absoluteDrive(double x, double y, double rotation) {
         drive.cartesian(x, y, rotation);
     }
 
+    /**
+     * Provides a robot-oriented control scheme that accepts raw joystick inputs.
+     * @param x The joystick's X (roll) axis, a value in [-1.0..1.0]. Translates to leftward/rightward motion.
+     * @param y The joystick's Y (pitch) axis, a value in [-1.0..1.0]. Translates to forward/backward motion.
+     * @param rotation The joystick's twist (yaw) axis, a value in [-1.0..1.0]. Translates to rotation of the robot.
+     */
     public void relativeDrive(double x, double y, double rotation) {
         y *= -1;
         drive.polar(Math.sqrt(x * x + y * y), Math.toDegrees(Math.atan2(x, y)), rotation);
     }
 
+    /**
+     * Provides a low-speed robot-oriented control scheme for fine control.
+     * @param angle The angle (in degrees) at which the robot should drive.
+     */
     public void trimDrive(int angle) {
         drive.polar(DRIVE_TRIM_SPEED, angle, 0.0);
     }
 
+    /**
+     * Gets the reading from the heading sensor.
+     * @return An angle in degrees describing the robot's heading. 0 is calibrated toward the far end of the field, and
+     *         clockwise is positive.
+     */
     public double getHeading() {
         return heading.getHeading();
     }
 
+    /**
+     * Sets the robot's current heading to zero. All future {@link #getHeading()} calls will be relative to the new
+     * calibration.
+     */
     public void zeroHeading() { heading.zero(); }
 }
