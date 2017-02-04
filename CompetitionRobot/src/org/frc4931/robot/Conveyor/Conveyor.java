dc27c809b8a2eb63545343ede69568fde94e4565
James Crane <james@cranesnest.us>
2017-02-03 20:51:21 -0600

    Added comments for Conveyor.java

--- CompetitionRobot/src/org/frc4931/robot/Conveyor/Conveyor.java
+++ CompetitionRobot/src/org/frc4931/robot/Conveyor/Conveyor.java
@@ -15,21 +15,36 @@ public class Conveyor implements Requirable{
     private final Controller shooter;
     private Motor intake;
 
+    /**
+     * Constructs a new Conveyor subsystem given a motor controller and a motor.
+     * @param shooter The Controller that is responsible for controlling the shooting mechanism.
+     * @param intake The Motor that is responsible for driving the conveyor intake.
+     */
     public Conveyor(Controller shooter, Motor intake) {
         this.shooter = shooter;
         this.intake = intake;
     }
 
+    /**
+     * Turns on the Intake and sets the Shooter to a low speed suitable for collecting balls.
+     */
     public void collect () {
         intake.setSpeed(INTAKE_SPEED);
         shooter.withTarget(SHOOTER_SPEED_IDLE);
     }
 
+    /**
+     * Turns on the Intake and sets the Shooter to a given speed.
+     * @param speedRPM The speed of the Shooter in RPM.
+     */
     public void shoot (double speedRPM) {
         intake.setSpeed(INTAKE_SPEED);
         shooter.withTarget(speedRPM);
     }
 
+    /**
+     * Stops the Conveyor mechanism.
+     */
     public void stop () {
         shooter.withTarget(0);
         intake.stop();
