57ddfee34a88d02f62623aedf7db7b7fb25675f7
christianmeinz <christian@meinzeit.com>
2017-02-17 17:26:02 -0600

    Add files via upload

--- ClimberSubSystem.java
+++ ClimberSubSystem.java
@@ -6,7 +6,7 @@ import org.strongback.components.Switch;
 /**
  * Created by christianmeinz1516 on 2/14/17
  */
-public class ClimberSubSystem{
+public class ClimberSubSystem {
     private final Motor climbPaddle;
     private final Switch horizontal, vertical, touchPad;
 	
@@ -16,7 +16,7 @@ public class ClimberSubSystem{
      * @param vertical    Trip sensor for detecting vertical
      * @param touchPad    Trip sensor for detecting touch pad
      */
-    public ClimberSubSystem(Motor climbPaddle, Switch horizontal, Switch vertical, Switch touchPad){
+    public ClimberSubSystem(Motor climbPaddle, Switch horizontal, Switch vertical, Switch touchPad) {
         this.climbPaddle = climbPaddle;
 		
         this.horizontal = horizontal;
@@ -27,35 +27,41 @@ public class ClimberSubSystem{
     /*
      * Starts the climber motor at a speed of 1
      */
-    public void start(){
+    public void start() {
         climbPaddle.setSpeed(1.0);
     }
 	
     /*
      * Stops the climber motor
      */
-    public void stop(){
+    public void stop() {
         climbPaddle.stop();
     }
 	
     /*
      * Returns if the touch pad has been triggered
+     * 
+     * @return Gets the boolean triggered reading from the touch pad sensor.
      */
-    public boolean hasScored(){
+    public boolean hasScored() {
         return touchPad.isTriggered();
     }
 	
     /*
      * Returns if the horizontal switch has been triggered
+     * 
+     * @return Gets the boolean triggered reading from the horizontal sensor.
      */
-    public boolean isHorizontal(){
+    public boolean isHorizontal() {
         return horizontal.isTriggered();
     }
 	
     /*
      * Returns if the vertical switch has been triggered
+     * 
+     * @return Gets the boolean triggered reading from the vertical sensor.
      */
-    public boolean isVertical(){
+    public boolean isVertical() {
         return vertical.isTriggered();
     }
     
